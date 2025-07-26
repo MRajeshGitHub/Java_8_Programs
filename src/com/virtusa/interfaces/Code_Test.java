b1344f5c412474a9491e7f04dc870f35305da32f
Rajesh <rajeshyaboo2008@gmail.com>
2025-07-26 12:45:10 +0530

    VS-BT-Java8-02-"Interface variable can,t be modified".

---
+++ src/com/virtusa/interfaces/Code_Test.java
@@ -0,0 +1,10 @@
+package com.virtusa.interfaces;
+
+public class Code_Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("a value is : "+Code.a);
+		//Code.a=20;//error finale variable will not be modified
+	}
+}
